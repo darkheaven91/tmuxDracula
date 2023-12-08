6bc6874cad1ae7466f15f5d4f5e2fd26ce4d76a4
darkheaven91 <105404991+darkheaven91@users.noreply.github.com>
2023-12-08 16:30:21 -0500

    Update get-ip.sh

--- scripts/get-ip.sh
+++ scripts/get-ip.sh
@@ -1,3 +1,3 @@
-INTERFACE=$(ip route | awk '/default/ {print $5}')
-ip a show $INTERFACE | grep -m 1 inet | awk '{print $2}' | cut -d / -f 1
-
+INTERFACE=$(ip route | head -n 1 | awk '/default/ {print $5}')
+IP=$(ip a show $INTERFACE | grep -m 1 inet | awk '{print $2}' | cut -d / -f 1)
+echo "$INTERFACE : $IP"
