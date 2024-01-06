261291a4f4967dace012ba4bc7b70e65b0a89f67
darkheaven91 <105404991+darkheaven91@users.noreply.github.com>
2024-01-06 02:40:40 -0500

    Update get-ip.sh

--- scripts/get-ip.sh
+++ scripts/get-ip.sh
@@ -1,3 +1,12 @@
-INTERFACE=$(ip route | head -n 1 | awk '/default/ {print $5}')
-IP=$(ip a show $INTERFACE | grep -m 1 inet | awk '{print $2}' | cut -d / -f 1)
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux
+    INTERFACE=$(ip route | head -n 1 | awk '/default/ {print $5}')
+    IP=$(ip a show $INTERFACE | grep -m 1 inet | awk '{print $2}' | cut -d / -f 1)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    INTERFACE=$(route get default | grep interface | awk '{print $2}')
+    IP=$(ifconfig $INTERFACE | awk '/inet / {print $2}')
+
 echo "$INTERFACE : $IP"
